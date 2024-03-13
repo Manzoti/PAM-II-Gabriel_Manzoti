c7dc8b9eac7bd95350dc33b6c3b23f1e624104b7
Gabriel Manzoti <gabriel.zotis@gmail.com>
2024-03-13 08:31:20 -0300

    Foi criado o Arquivo para o conseito de variaveis

---
+++ Aula 01/funcoes.js
@@ -0,0 +1,11 @@
+let num1 = 2
+const num2 = 3
+var msg = "Olá amigos! Esse é o número:"
+console.log(msg , num1)
+console.log(num1+num2)
+console.log(typeof num1)
+console.log(typeof msg)
+
+
+// para dar o espaço entre a escrita e o número dois tem 2 jeitos 
+//só dar o espaço ou no console contatenar com ,
