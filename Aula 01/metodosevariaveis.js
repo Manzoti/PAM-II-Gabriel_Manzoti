4be6c0cbed5b951a7592f3a7061183626b6aafa1
Gabriel de Castro Manzoti <gabriel.zotis@gmail.com>
2024-04-03 08:37:52 -0300

    AUla 2 sobre funções e métodos

---
+++ Aula 01/metodosevariaveis.js
@@ -0,0 +1,67 @@
+var variavel1 = "olÃ¡";
+let variavel2 = 3;
+const variavel3 = 8;
+
+console.log(
+    "OlÃ¡ mundo!"
+    ); 
+
+console.log(variavel1, "mundo!");
+console.log(variavel1 + "mundo!");
+console.log(`${variavel1} mundo!`)
+
+
+
+
+
+
+
+
+
+
+
+// console.log(variavel1 + "mundo");
+// console.log(variavel1, "mundo");
+// console.log(variavel1+ " Turminha do "+ variavel2+ "Âº ano")
+// console.log(variavel2 + variavel3)
+// console.log(typeof variavel1)
+// console.log(typeof variavel2)
+// variavel1 = 4
+// console.log(typeof variavel1)
+// const pessoa = {
+//     nome: "JoÃ£o",
+//     signo: "EscorpiÃ£o",
+//     idade: 35
+// }
+// console.log(pessoa)
+// console.log(
+//     "Nome:", pessoa.nome,
+//     "\nIdade:", pessoa.idade
+// )
+// let cachorros = ["Bug", "Zeus", "Zaratrusta"]
+// console.log(cachorros[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // Formato JSON - JavaScript Notation Object
+// // console.log(obj.nome, obj.signo)
+// // console.log(typeof obj)
