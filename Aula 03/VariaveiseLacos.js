ba5fc26979b1f74bcaec78495967ad8ccfb3d3d8
Gabriel Manzoti <gabriel.zotis@gmail.com>
2024-04-10 08:34:18 -0300

    Aula 03 sobre arrays e objeto

---
+++ Aula 03/VariaveiseLacos.js
@@ -0,0 +1,29 @@
+let n = 2
+let t = "oi"
+
+console.log (typeof n)
+console.log (typeof t)
+
+let a = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+//Comoça a contar do 0
+
+console.log (typeof a)
+
+let o = {nome: "Gabriel", idade: 17} 
+
+// JSON
+//JavaScript Objetct Notation
+
+console.log (typeof o)
+
+console.log(a)
+console.log(a [6])
+
+a.forEach( (el) => 
+    console.log(`Elemento: ${el}`)
+
+)
+
+console.log(o)
+console.log(o.nome, "tem", o.idade)
