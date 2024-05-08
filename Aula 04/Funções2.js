994d9cb1e4f4a546e5a55fcf0d98e05669dbcee4
Gabriel Manzoti <gabriel.zotis@gmail.com>
2024-05-08 08:28:20 -0300

    Aula de otimização de código

---
+++ Aula 04/Funções2.js
@@ -0,0 +1,44 @@
+/*const TesteImparPar = (n) => {
+    let calculoTeste = n % 2
+
+    if (calculoTeste == 1) {
+        return "Número Impar"
+    } else {
+        return "Número Par"
+    }
+}
+
+console.log(TesteImparPar(17))
+console.log(TesteImparPar(64))
+console.log(TesteImparPar(1500))*/
+
+//--------------------------------------------------------------------
+
+/*const TesteImparPar = (n) => {
+    let calculoTeste = n % 2
+
+    if (calculoTeste == 1) {
+        return "Número Impar"
+    }
+
+    return "Número Par"
+}
+
+console.log(TesteImparPar(17))
+console.log(TesteImparPar(64))
+console.log(TesteImparPar(1500))*/
+
+//--------------------------------------------------------------------
+
+const TesteImparPar = (n) => {
+    let calculoTeste = n % 2
+
+    return (calculoTeste == 1) ? "Número impar" : "Número par"
+}
+
+console.log(TesteImparPar(17))
+console.log(TesteImparPar(64))
+console.log(TesteImparPar(1500))
+
+
+// Quanto menor o código melhor
