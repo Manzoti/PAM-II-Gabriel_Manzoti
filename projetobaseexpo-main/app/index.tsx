cc117fbe9793d7019713d6fd39bb1bd4f55b41ee
Gabriel Manzoti <gabriel.zotis@gmail.com>
2024-06-20 15:24:30 -0300

    Projeto finalizado

--- projetobaseexpo-main/app/index.tsx
+++ projetobaseexpo-main/app/index.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
 import { useState, useRef } from 'react';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from 'react-native-view-shot';
+import { StyleSheet, View,  Platform  } from 'react-native';
 
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 
+import domtoimage from 'dom-to-image';
 import EmojiSticker from '../components/EmojiSticker';
 import EmojiList from '../components/EmojiList';
 import EmojiPicker from "../components/EmojiPicker";
@@ -55,7 +56,7 @@ export default function App() {
     requestPermission();
   }
   
-  const onSaveImageAsync = async () => {
+  /*const onSaveImageAsync = async () => {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -69,8 +70,41 @@ export default function App() {
     } catch (e) {
       console.log(e);
     }
+  };*/
+
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+  
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+    }
   };
   
+
   return (
    <GestureHandlerRootView style={styles.container}>
     <View style={styles.container}>
@@ -97,7 +131,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
    </GestureHandlerRootView>
   );
