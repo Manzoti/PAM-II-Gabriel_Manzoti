eb8e4b2f37614a8b24807f4f4f8a8ae7cdd6c8ce
Gabriel Manzoti <gabriel.zotis@gmail.com>
2024-10-09 08:34:12 -0300

    Aula 8

---
+++ Leitura/app/index.tsx
@@ -0,0 +1,53 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAu9DmAsfgWCNtgMXO0CsrU-nUS7Rlzj1k",
+  authDomain: "meuprimeirofirebase-176d1.firebaseapp.com",
+  projectId: "meuprimeirofirebase-176d1",
+  storageBucket: "meuprimeirofirebase-176d1.appspot.com",
+  messagingSenderId: "646357232434",
+  appId: "1:646357232434:web:b1d81775b341672be7052b"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+export default function App() {
+  const [nomes, setNomes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const nomesCollection = firebase.firestore().collection('Nomes');
+      const snapshot = await nomesCollection.get();
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+
+      setNomes(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Lista de Nomes:</Text>
+      <FlatList
+        data={nomes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.Nome} {item.Sobrenome}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
